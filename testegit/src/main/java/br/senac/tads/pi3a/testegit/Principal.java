e794d5b13c4058167d5ef578c7a1fd50cffdbfe6
isaias.lbrito <isaias.lbrito@CAS555506W0708.senacsp.edu.br>
2017-02-24 20:38:22 -0300

    ostrar 10x a mensagem

--- testegit/src/main/java/br/senac/tads/pi3a/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3a/testegit/Principal.java
@@ -1,9 +1,11 @@
-
 package br.senac.tads.pi3a.testegit;
 
-
 public class Principal {
+
     public static void main(String[] args) {
-        System.err.println("hello word senac ");
+        for (int i = 0; i < 10; i++) {
+            System.err.println(i+ " hello word senac ");
+            
+        }
     }
 }
