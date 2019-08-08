0ae834fb92b8a06df574ade144708e50f0bd1e1d
Isaias Brito <isaias9713@hotmail.com>
2019-08-08 13:17:20 +0000

    teste 1.2

--- testegit/src/main/java/br/senac/tads/pi3a/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3a/testegit/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.err.println(i+ " hello word senac ");
+            System.err.println(i+ " hello word senac 1.2");
             
         }
     }
